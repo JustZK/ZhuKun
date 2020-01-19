c6c8751ada0c215f35ec579312fee5fddc8dccb0
JustZK <zk199519@gmail.com>
2020-01-19 11:01:34 +0800

    新建ping

---
+++ common/src/main/java/com/zk/common/utils/PingUtil.kt
@@ -0,0 +1,18 @@
+package com.zk.common.utils
+
+import android.content.Context
+
+class PingUtil {
+    class PingEntity(context: Context, ip: String, pingCount: Int, pingWaitTime: Int) {
+        // init
+        val context = context
+        val ip = ip //Ping的地址
+        val pingCount = pingCount //Ping的次数
+        val pingWaitTime = pingWaitTime //等待时间/设置超时的时间
+        //return
+        val resultBuffer: StringBuffer? = null //ping得到的数据
+        val pingTime: String? = null //ping通的时间
+        val result = false //结果
+    }
+
+}
