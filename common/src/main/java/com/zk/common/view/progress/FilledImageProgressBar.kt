369d238c8a14b9f98340987f39fd402add1a315e
JustZK <zk199519@gmail.com>
2020-04-20 14:15:15 +0800

    完成FilledImageProgressBar

--- common/src/main/java/com/zk/common/view/progress/FilledImageProgressBar.kt
+++ common/src/main/java/com/zk/common/view/progress/FilledImageProgressBar.kt
@@ -21,6 +21,9 @@ class FilledImageProgressBar : View {
     // 画圆环背景图
     @DrawableRes
     private var mRingBackgroundImage: Int? = null
+    // 画圆环背景图和圈的距离
+    @Size
+    private var mRingBackgroundImageDistance: Float? = null
     // 画圆环背景图颜色
     @ColorRes
     private var mRingBackgroundImageColor: Int? = null
@@ -55,9 +58,6 @@ class FilledImageProgressBar : View {
     // 字的高度
     private var mTextHeight = 0f
 
-    //    private var mRectF: RectF? = null
-//    private var mBitmap: Bitmap? = null
-
     constructor(context: Context, attributeSet: AttributeSet) : super(context, attributeSet) {
         val typeArray = context.theme.obtainStyledAttributes(attributeSet,
                 R.styleable.FilledImageProgressBar, 0, 0)
@@ -65,6 +65,8 @@ class FilledImageProgressBar : View {
                 R.styleable.FilledImageProgressBar_ringBackgroundColor, R.color.md_blue_100)
         mRingBackgroundImage = typeArray.getResourceId(
                 R.styleable.FilledImageProgressBar_ringBackgroundImage, R.drawable.ic_fingerprint)
+        mRingBackgroundImageDistance = typeArray.getFloat(
+                R.styleable.FilledImageProgressBar_ringBackgroundImageDistance, -1f)
         mRingBackgroundImageColor = typeArray.getResourceId(
                 R.styleable.FilledImageProgressBar_ringBackgroundImageColor, R.color.md_blue_grey_200)
         mRingColor = typeArray.getResourceId(
@@ -78,47 +80,61 @@ class FilledImageProgressBar : View {
         mTotalProgress = typeArray.getFloat(
                 R.styleable.FilledImageProgressBar_totalProgress, 100f)
         mCurrentProgress = typeArray.getFloat(
-                R.styleable.FilledImageProgressBar_currentProgress, 25f)
+                R.styleable.FilledImageProgressBar_currentProgress, 0f)
         mShowImage = typeArray.getBoolean(
                 R.styleable.FilledImageProgressBar_showImage, true)
-        initVariable()
     }
 
     private fun initVariable() {
-        mRingPaint = Paint()
-        mRingPaint!!.isAntiAlias = true
-        mRingPaint!!.isDither = true
-        mRingPaint!!.color = resources.getColor(mRingColor!!)
-        mRingPaint!!.style = Paint.Style.STROKE
-        mRingPaint!!.strokeCap = Paint.Cap.ROUND
-        mRingPaint!!.strokeWidth = mRingEdgeWidth!!
+        if(mRingRadius == -1f){
+            mRingRadius = width / 2f - mRingEdgeWidth!!
+        }
+        if (mRingBackgroundImageDistance == -1f){
+            mRingBackgroundImageDistance = mRingEdgeWidth!! * 3
+        }
 
-        mRingBackgroundImagePaint = Paint(Paint.ANTI_ALIAS_FLAG)
-        mRingBackgroundImagePaint!!.isFilterBitmap = true
-        mRingBackgroundImagePaint!!.isDither = true
+        if (mRingPaint == null) {
+            mRingPaint = Paint()
+            mRingPaint!!.isAntiAlias = true
+            mRingPaint!!.isDither = true
+            mRingPaint!!.color = resources.getColor(mRingColor!!)
+            mRingPaint!!.style = Paint.Style.STROKE
+            mRingPaint!!.strokeCap = Paint.Cap.ROUND
+            mRingPaint!!.strokeWidth = mRingEdgeWidth!!
+        }
 
-        mRingBackgroundPaint = Paint()
-        mRingBackgroundPaint!!.isAntiAlias = true
-        mRingBackgroundPaint!!.isDither = true
-        mRingBackgroundPaint!!.color = resources.getColor(mRingBackgroundColor!!)
-        mRingBackgroundPaint!!.style = Paint.Style.STROKE
-        mRingBackgroundPaint!!.strokeCap = Paint.Cap.ROUND
-        mRingBackgroundPaint!!.strokeWidth = mRingEdgeWidth!!
+        if (mRingBackgroundImagePaint == null) {
+            mRingBackgroundImagePaint = Paint(Paint.ANTI_ALIAS_FLAG)
+            mRingBackgroundImagePaint!!.isFilterBitmap = true
+            mRingBackgroundImagePaint!!.isDither = true
+        }
 
-        mTextPaint = Paint()
-        mTextPaint!!.isAntiAlias = true
-        mTextPaint!!.style = Paint.Style.FILL
-        mTextPaint!!.color = resources.getColor(mRingBackgroundColor!!)
-        mTextPaint!!.textSize = (mRingRadius!! / 2)
-        val fm: Paint.FontMetrics = mTextPaint!!.fontMetrics
-        mTextHeight = fm.descent + abs(fm.ascent)
+        if (mRingBackgroundPaint == null) {
+            mRingBackgroundPaint = Paint()
+            mRingBackgroundPaint!!.isAntiAlias = true
+            mRingBackgroundPaint!!.isDither = true
+            mRingBackgroundPaint!!.color = resources.getColor(mRingBackgroundColor!!)
+            mRingBackgroundPaint!!.style = Paint.Style.STROKE
+            mRingBackgroundPaint!!.strokeCap = Paint.Cap.ROUND
+            mRingBackgroundPaint!!.strokeWidth = mRingEdgeWidth!!
+        }
+
+        if (!mShowImage && mTextPaint == null) {
+            mTextPaint = Paint()
+            mTextPaint!!.isAntiAlias = true
+            mTextPaint!!.style = Paint.Style.FILL
+            mTextPaint!!.color = resources.getColor(mRingBackgroundColor!!)
+            mTextPaint!!.textSize = (mRingRadius!! / 2)
+            val fm: Paint.FontMetrics = mTextPaint!!.fontMetrics
+            mTextHeight = fm.descent + abs(fm.ascent)
+        }
     }
 
     @SuppressLint("DrawAllocation")
     override fun onDraw(canvas: Canvas?) {
         super.onDraw(canvas)
         if (mCurrentProgress >= 0) {
-            if(mRingRadius == -1f) mRingRadius = width / 2f - mRingEdgeWidth!!
+            initVariable()
 
             val mRectF = RectF(width / 2 - mRingRadius!!,
                     height / 2 - mRingRadius!!,
@@ -141,10 +157,10 @@ class FilledImageProgressBar : View {
 //                // 计算上边位置
 //                val top = (width - mBitmap!!.height) / 2
 //            val mDestRect = Rect(left, top, left + mBitmap!!.width, top + mBitmap!!.height)
-                val mDestRect = RectF( mRingEdgeWidth!! * 10,
-                        mRingEdgeWidth!! * 10,
-                        width - mRingEdgeWidth!! * 10,
-                        height - mRingEdgeWidth!! * 10)
+                val mDestRect = RectF( mRingBackgroundImageDistance!!,
+                        mRingBackgroundImageDistance!!,
+                        width - mRingBackgroundImageDistance!!,
+                        height - mRingBackgroundImageDistance!!)
                 canvas.drawBitmap(mBitmap!!,
                         null, mDestRect,
                         mRingBackgroundImagePaint)
